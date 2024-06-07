1aa2f9e3c02011059058a3aae5be68192e4193d8
Việt Nguyễn <huntershadow221@gmail.com>
2024-06-07 22:38:57 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+
+document.addEventListener('click', () => {
+  const audio = document.getElementById('background-audio');
+  if (audio.paused) {
+    audio.play();
+  }
+});
